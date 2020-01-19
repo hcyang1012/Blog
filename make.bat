505e9eb238b59b324819cf86f50c02cafdf29b16
Heecheol Yang <heecheol.yang@outlook.com>
2020-01-19 16:26:47 +0900

    Silent! make.bat

--- make.bat
+++ make.bat
@@ -1,14 +1,14 @@
 
 
-rem  Minimal makefile for Sphinx documentation
-rem 
+@rem  Minimal makefile for Sphinx documentation
+@rem 
 
-rem  You can set these variables from the command line.
-set SPHINXOPTS =
-set SPHINXBUILD=sphinx-build
-set SOURCEDIR= .
-set BUILDDIR= _build
+@rem  You can set these variables from the command line.
+@set SPHINXOPTS =
+@set SPHINXBUILD=sphinx-build
+@set SOURCEDIR= .
+@set BUILDDIR= _build
 
-rem  Catch-all target: route all unknown targets to Sphinx using the new
-rem  "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
-%SPHINXBUILD% -M html "%SOURCEDIR%" "%BUILDDIR%" %SPHINXOPTS%
+@rem  Catch-all target: route all unknown targets to Sphinx using the new
+@rem  "make mode" option.  $(O) is meant as a shortcut for $(SPHINXOPTS).
+@%SPHINXBUILD% -M html "%SOURCEDIR%" "%BUILDDIR%" %SPHINXOPTS%
